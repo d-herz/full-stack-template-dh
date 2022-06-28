32501d790a49dfbad4e3ec1e49a5f72202936caa
Daniel Hyres <danhyres@gmail.com>
2022-06-28 19:23:53 -0400

    Added port and mongo connections

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
-
 const cors = require('cors')
-
 const MongoClient = require('mongodb').MongoClient
-
 require('dotenv').config()
+
 
 let db,
   dbconnectionString = process.env.DB_STRING,
@@ -17,4 +15,8 @@ MongoClient.connect(dbconnectionString)
     console.log('Connected to DB YO!')
     db = client.db(dbName)
     collection = db.collection('quotes')  //this is a unique collection name to the breaking-bad DB (ie update this to match future projects)
+  })
+
+  app.listen( process.env.PORT || PORT, ()=> {
+    console.log(`Server is running on port = ${ process.env.PORT || PORT }`)
   })
