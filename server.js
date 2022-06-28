0b62bb120ce86557eed0cd3a7e143121dc2c177b
Daniel Hyres <danhyres@gmail.com>
2022-06-28 17:52:24 -0400

    Added variables and mongo connection

--- server.js
+++ server.js
@@ -6,3 +6,15 @@ const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 
 require('dotenv').config()
+
+let db,
+  dbconnectionString = process.env.DB_STRING,
+  dbName = 'breaking-bad-quotes', //update this in future projects 
+  collection
+
+MongoClient.connect(dbconnectionString)
+  .then(client => {
+    console.log('Connected to DB YO!')
+    db = client.db(dbName)
+    collection = db.collection('quotes')  //this is a unique collection name to the breaking-bad DB (ie update this to match future projects)
+  })
